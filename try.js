7631c4a1c72a264728eee84ed5158319fe24e0b7
Tumbo Lobi John <johnlobiit@gmail.com>
2022-06-14 13:53:35 +0300

    ternary operator

---
+++ try.js
@@ -0,0 +1,18 @@
+let age = 25;
+let year = 2021;
+console.log(age, year);
+
+const points = 100;
+console.log(points)
+
+var car = "mercedez";
+document.write(car)
+
+let firstName = "John";
+let lastName = "Depp";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
+
