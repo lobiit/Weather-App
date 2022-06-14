082b6729148fc26c54afd4972d9a74358fb67945
Tumbo Lobi John <johnlobiit@gmail.com>
2022-06-14 12:46:22 +0300

    Updating the location

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -21,10 +21,5 @@ const getCity = async (city) => {
 
     return data[0];
 };
-getCity('Nairobi').then( data => {
-    return getWeather(data.Key);
-}).then(data => {
-    console.log(data)
-})
-    .catch(err => console.log(err));
+
 
