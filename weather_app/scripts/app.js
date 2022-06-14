cb392fcc171fae21fc1508fe9bab7e956c37dbdb
Tumbo Lobi John <johnlobiit@gmail.com>
2022-06-14 13:11:14 +0300

    Updating the user interface

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -6,7 +6,19 @@ const updateUI = (data) => {
     const cityDets = data.cityDets;
     const weather = data.weather;
 
-    // update
+    // update details template
+    details.innerHTML = `  
+                <h5 class="my-3">${cityDets.EnglishName}</h5>
+                <div class="my-3">${weather.WeatherText}</div>
+                <div class="display-4 my-4">
+                    <span>${weather.Temperature.Metric.Value}</span>
+                    <span>&deg;C</span>
+                </div>
+    `;
+    // remove the d-none class if present
+    if (card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
 };
 
 const updateCity = async (city) =>{
